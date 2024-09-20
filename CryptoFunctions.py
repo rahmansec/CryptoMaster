ec38afd8ab6cadfa123043d3c4d2395607933ad5
Mr-Hosseinzadeh <rahman.h.z.2023@gmail.com>
2024-09-20 13:37:51 +0330

    add Encrypt_Class:     add AES_encrypt functions add Decrypt_Class:    add AES_decrypt functions

--- CryptoFunctions.py
+++ CryptoFunctions.py
@@ -4,6 +4,12 @@ from Crypto.Hash import RIPEMD160, MD5, SHA1, SHA224, SHA256, SHA384, SHA512
 import base64
 import urllib
 import html
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
+from Crypto.Util.Padding import pad, unpad
+from rich.prompt import Prompt
+import typer 
+
 
 class Hashs_Class:
     def md5(self, text: str, salt: str = ""):
@@ -83,3 +89,34 @@ class Decode_Class():
     def html_decode(self, encoded_text: str):
         decoded = html.unescape(encoded_text)
         return decoded        
+    
+    
+
+
+class Encrypt_Class:
+    def AES_encrypt(self,input:str,password:str):
+        
+        data = input.encode()
+        hash = Hashs_Class().sha256(password)
+        key = bytes.fromhex(hash)
+        iv = get_random_bytes(16) 
+        cipher = AES.new(key, AES.MODE_CBC, iv) 
+        ciphertext = cipher.encrypt(pad(data, AES.block_size)) 
+        combined = iv + ciphertext
+        return combined.hex()
+
+class Decrypt_Class:
+    def AES_decrypt(self, ciphertext:str,password:str):
+        combined = bytes.fromhex(ciphertext)
+        iv = combined[:16] 
+        ciphertext_bytes = combined[16:] 
+        hash = Hashs_Class().sha256(password)
+        key = bytes.fromhex(hash)
+        try:
+            decipher = AES.new(key, AES.MODE_CBC, iv) 
+            plaintext = unpad(decipher.decrypt(ciphertext_bytes), AES.block_size) 
+            return plaintext.decode()
+        except:
+            return typer.secho("Faild...",fg="red")
+
+    
--- Menu.py
+++ Menu.py
@@ -1,82 +1,129 @@
 from rich.console import Console
-from rich.prompt import Prompt
+from rich.prompt import Prompt,IntPrompt,Confirm
 from rich.table import Table
 from CryptoFunctions import *
 
 
 console = Console()
-
+encrypt = Encrypt_Class()
+decrypt=Decrypt_Class()
 hashs = Hashs_Class()
 encoder = Encode_Class()
 decoder = Decode_Class()
 
 def show_menu():
-    console.print("Please select an option:", style="bold green")
-    table = Table(show_header=True, header_style="bold magenta")
-    table.add_column("Option", justify="center")
-    table.add_column("Description")
+    # console.print("Please select an option:", style="bold green")
+    hash_table = Table(show_header=True, header_style="bold magenta",title="Hash")
+    hash_table.add_column("Option", justify="center")
+    hash_table.add_column("Description")
     
-    table.add_row("1", "MD5 Hashing")
-    table.add_row("2", "Bcrypt Hashing")
-    table.add_row("3", "RIPEMD Hashing")
-    table.add_row("4", "SHA-1 Hashing")
-    table.add_row("5", "SHA-224 Hashing")
-    table.add_row("6", "SHA-256 Hashing")
-    table.add_row("7", "SHA-384 Hashing")
-    table.add_row("8", "SHA-512 Hashing")
-    table.add_row("9", "Base64 Encoding")
-    table.add_row("10", "URL Encoding")
-    table.add_row("11", "HTML Encoding")
-    table.add_row("12", "Base64 Decoding")
-    table.add_row("13", "URL Decoding")
-    table.add_row("14", "HTML Decoding")
-    table.add_row("0", "Exit")
+    hash_table.add_row("1", "MD5 Hashing")
+    hash_table.add_row("2", "Bcrypt Hashing")
+    hash_table.add_row("3", "RIPEMD Hashing")
+    hash_table.add_row("4", "SHA-1 Hashing")
+    hash_table.add_row("5", "SHA-224 Hashing")
+    hash_table.add_row("6", "SHA-256 Hashing")
+    hash_table.add_row("7", "SHA-384 Hashing")
+    hash_table.add_row("8", "SHA-512 Hashing")
+    
+    encode_table = Table(show_header=True, header_style="bold magenta",title="Encode")
+    encode_table.add_column("Option", justify="center")
+    encode_table.add_column("Description")
+    encode_table.add_row("9", "Base64 Encoding")
+    encode_table.add_row("10", "URL Encoding")
+    encode_table.add_row("11", "HTML Encoding")
+    
+    
+    decode_table = Table(show_header=True, header_style="bold magenta",title="Decode")
+    decode_table.add_column("Option", justify="center")
+    decode_table.add_column("Description")
+    decode_table.add_row("12", "Base64 Decoding")
+    decode_table.add_row("13", "URL Decoding")
+    decode_table.add_row("14", "HTML Decoding")
+    
+    
+    encrypt_table = Table(show_header=True, header_style="bold magenta",title="Encrypt")
+    encrypt_table.add_column("Option", justify="center")
+    encrypt_table.add_column("Description")
+    encrypt_table.add_row("15", "AES Encrypt")
 
-    console.print(table)
+    decrypt_table= Table(show_header=True, header_style="bold magenta",title="Decrypt")
+    decrypt_table.add_column("Option", justify="center")
+    decrypt_table.add_column("Description")
+    decrypt_table.add_row("16", "AES Decrypt")
+    
+    other_table= Table(show_header=True, header_style="bold magenta",title="Other")
+    other_table.add_column("Option", justify="center")
+    other_table.add_column("Description")
+    other_table.add_row("0", "Exit")
+
+    console.print(hash_table)
+    console.print(encode_table)
+    console.print(decode_table)
+    console.print(encrypt_table)
+    console.print(decrypt_table)
+    console.print(other_table)
 
 def process_choice(choice):
     if choice == "1":
         text = Prompt.ask("Enter text to hash with MD5")
-        result = hashs.md5(text)
+        salt = Prompt.ask("Enter Salt",default="")
+        result = hashs.md5(text,salt)
     elif choice == "2":
         text = Prompt.ask("Enter text to hash with Bcrypt")
-        result = hashs.bcrypt(text)
+        rounds = IntPrompt.ask("Enter Round",default=12)
+        prefix = Prompt.ask("Enter Prefix",default="2b")
+        result = hashs.bcrypt(text,rounds,prefix)
     elif choice == "3":
         text = Prompt.ask("Enter text to hash with RIPEMD")
-        result = hashs.ripemd(text)
+        salt = Prompt.ask("Enter Salt",default="")
+        result = hashs.ripemd(text,salt)
     elif choice == "4":
         text = Prompt.ask("Enter text to hash with SHA-1")
-        result = hashs.sha1(text)
+        salt = Prompt.ask("Enter Salt",default="")
+        result = hashs.sha1(text,salt)
     elif choice == "5":
         text = Prompt.ask("Enter text to hash with SHA-224")
+        salt = Prompt.ask("Enter Salt",default="")
         result = hashs.sha224(text)
     elif choice == "6":
         text = Prompt.ask("Enter text to hash with SHA-256")
-        result = hashs.sha256(text)
+        salt = Prompt.ask("Enter Salt",default="")
+        result = hashs.sha256(text,salt)
     elif choice == "7":
         text = Prompt.ask("Enter text to hash with SHA-384")
-        result = hashs.sha384(text)
+        salt = Prompt.ask("Enter Salt",default="")
+        result = hashs.sha384(text,salt)
     elif choice == "8":
         text = Prompt.ask("Enter text to hash with SHA-512")
-        result = hashs.sha512(text)
+        salt = Prompt.ask("Enter Salt",default="")
+        result = hashs.sha512(text,salt)
     elif choice == "9":
         text = Prompt.ask("Enter text to Base64 encode")
         result = encoder.base64_encode(text)
     elif choice == "10":
         text = Prompt.ask("Enter text to URL encode")
         result = encoder.url_encode(text)
-    elif choice == "12":
+    elif choice == "11":
         text = Prompt.ask("Enter text to HTML encode")
         result = encoder.html_encode(text)
-    elif choice == "13":
+    elif choice == "12":
         text = Prompt.ask("Enter Base64 encoded text to decode")
         result = decoder.base64_decode(text)
-    elif choice == "14":
+    elif choice == "13":
         text = Prompt.ask("Enter URL encoded text to decode")
         result = decoder.url_decode(text)
-    elif choice == "16":
+    elif choice == "14":
         text = Prompt.ask("Enter HTML encoded text to decode")
         result = decoder.html_decode(text)
+    elif choice == "15":
+        text = Prompt.ask("Enter Text")
+        password = Prompt.ask("Password", password=True)
+        result = encrypt.AES_encrypt(text,password)
+    elif choice == "16":
+        ciphertext = Prompt.ask("Enter ciphertext(hex)")
+        password = Prompt.ask("Password", password=True)
+        result = decrypt.AES_decrypt(ciphertext,password)
     elif choice == "0":
         console.print("Exiting...", style="bold red")
         return False
@@ -85,4 +132,6 @@ def process_choice(choice):
         return True
 
     console.print(f"Result: [bold yellow]{result}[/bold yellow]")
-    return True
+    if Confirm.ask("Continue?"):
+        return True
+    return False
